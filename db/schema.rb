49261db5014ded030203ac32e427041e80893eb8
Troy Anderson <the.inventor@gmail.com>
2022-03-30 12:00:09 -0700

    add devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_182319) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_184951) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_30_182319) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
