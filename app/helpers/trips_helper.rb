b4b759f54370631bdad185ca0e83b71cb8ffec2a
Troy Anderson <the.inventor@gmail.com>
2022-04-10 21:56:24 -0700

    fix for firefox - woot blair

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -2,6 +2,6 @@ module TripsHelper
 
   def local_time timestamp
     # should build html like; <span data-controller="local-time"><%= trip.created_at %></span>
-    content_tag(:span, timestamp, data: {controller: 'local-time'})
+    content_tag(:span, timestamp.iso8601, data: {controller: 'local-time'})
   end
 end
