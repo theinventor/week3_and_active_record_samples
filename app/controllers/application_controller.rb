eb2cf4021d2afc3b7f7809bac6523e155c65b805
Troy Anderson <the.inventor@gmail.com>
2022-05-04 10:21:13 -0700

    add some puts to see whats happening, add a fallback path that was missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,18 @@ class ApplicationController < ActionController::Base
       cookies[:cool_shopping_cart] = @cart.uuid
     else
       if cookies[:cool_shopping_cart].present?
+        puts "they have a cart cookie"
         if @cart = Cart.their_cart_still_good?(uuid: cookies[:cool_shopping_cart])
           puts "good guest cart"
         else
+          puts "needed to  make a new guest cart - their id was probably invalid"
           @cart = Cart.create
           cookies[:cool_shopping_cart] = @cart.uuid
         end
+      else
+        puts "no cookie, lets make a cart for a guest"
+        @cart = Cart.create
+        cookies[:cool_shopping_cart] = @cart.uuid
       end
     end
   end
